60712a2cd9a9a89a7750b66cc9e83f1f8b340e59
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2024-03-14 14:39:37 +0300

    add header

---
+++ src/components/Header/slyled.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled"
+import { AppBar } from "@mui/material"
+
+const HeaderContainer = styled(AppBar)({
+  width: "100%",
+  alignItems: "center",
+  display: "flex",
+  padding: "20px",
+  backgroundColor: "#42424269",
+})
+
+export default HeaderContainer
